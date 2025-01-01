2fd98a75b1104f12d2b8d84c3bc0a63e5767b132
eli <ebezhiasvhili@gmail.com>
2025-01-02 01:01:26 +0400

    add gitginore

--- finance_app_backend2/settings.py
+++ finance_app_backend2/settings.py
@@ -25,21 +25,25 @@ SECRET_KEY = 'django-insecure-di1ax7_%(5worg-9+=hwslar5#-2q!x^-ftke&qn08#+w73-+t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "api",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'finance_app_backend2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -73,13 +77,6 @@ WSGI_APPLICATION = 'finance_app_backend2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -122,3 +119,40 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'financeapp',
+        'USER': 'pgadmin_user',
+        'PASSWORD': 'Makaka12',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+
+}
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",    # Vite default
+    "http://localhost:5174",  # Vite default
+    "http://localhost:5175",  # Vite default
+    "http://localhost:5176",  # Vite default
+    "http://localhost:3000",  # React app
+
+]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+CORS_ALLOW_CREDENTIALS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Or your email provider's SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'elisabed.bezhiashvili.1@iliauni.edu.ge'  # Your email address
+EMAIL_HOST_PASSWORD = 'zvmm acli nifx jcmd'
